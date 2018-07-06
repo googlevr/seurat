b6e6b9584d1f9a21aa04c7bb4d8285e115137dcc
Mike Feldstein <feldstein@fb.com>
2018-07-06 09:57:48 -0700

    Add minor gl version hint

--- seurat/viewer/butterfly/butterfly.cc
+++ seurat/viewer/butterfly/butterfly.cc
@@ -214,6 +214,7 @@ int main(int argc, char** argv) {
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
   window = glfwCreateWindow(kWindowWidth, kWindowHeight, "Butterfly", nullptr,
                             nullptr);
